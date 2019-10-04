a94088dc634c9a0668c4993c5100a81d35c01e86
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-10-04 16:26:09 -0400

    checkpoint

---
+++ app/controllers/user_books_controller.rb
@@ -0,0 +1,35 @@
+class UserBooksController < ApplicationController
+    before_action :authorized
+
+    def create
+        byebug
+        
+        # find or create book
+        # step 1: find by isbn
+        isbn_10 = params["bookData"]["identifiers"]["ISBN-10"]
+        isbn_13 = params["bookData"]["identifiers"]["ISBN-13"]
+        book = Book.find_by(isbn_10: isbn_10) || Book.find_by(isbn_13: isbn_13)
+
+        # step 2: if not found, create book
+        # also need to create the author(s) and AuthorBook joins
+        if !book
+            params["bookData"]["title"]
+            params["bookData"]["bookUrl"]
+            params["bookData"]["coverUrl"]
+            params["bookData"]["authors"]
+
+        end
+
+
+        # need to make author(s) and joins
+        # create UserBook
+        
+        # need user 
+        current_user
+        # activerecord serializer??
+    end
+    
+    def destroy
+        byebug
+    end
+end
--- db/migrate/20191001153451_create_books.rb
+++ db/migrate/20191001153451_create_books.rb
@@ -1,9 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
+      t.string :title
       t.string :isbn_10
       t.string :isbn_13
-      t.string :title
+      t.string :book_url
+      t.string :cover_url
       t.timestamps
     end
   end
