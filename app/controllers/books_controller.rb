72d2023bb186d724acceb4fa9d3d4322f16ba763
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-10-06 15:41:06 -0400

    update books#actives_only to make use of Book.only_books_with_active_user_books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,12 @@
 class BooksController < ApplicationController
 
     def index
-        render json: Book.all.map { |book| book.serialize(active: false) }
+        render json: Book.all.map { |book| book.serialize(only_active_user_books: false) }
     end
 
     def actives_only
         # via this action:
-        # user can see all books that have active UserBooks
-        render json: Book.books_with_actives_only 
+        # user can see all books that have active UserBooks (serialized)
+        render json: Book.only_books_with_active_user_books # is serializer too
     end
 end
